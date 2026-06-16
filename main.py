5213daad67e83d8cf6c9138730c505b6bc065d5b
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-16 23:01:29 +0530

    feat: add document ingestion pipeline with PyPDFLoader and Pinecone vector storage

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("./fullstack_guide.pdf")
+docs = loader.load()
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 
@@ -13,5 +17,5 @@ index = pc.Index("rag-index1")
 
 vector_store = PineconeVectorStore(embedding=embeddings, index=index)
 
-print(vector_store.add_texts(["ring"]))
+print(vector_store.add_documents(documents=docs))
 
