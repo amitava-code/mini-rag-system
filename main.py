def4451759e876e43ab372af425e6ec33965342f
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-16 20:27:29 +0530

    feat: connect Pinecone index with LangChain vector store for embeddings

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()
+import os
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+index = pc.Index("rag-index")
 
-vectors = embeddings.embed_query("ai")
+vector_store = PineconeVectorStore(embedding=embeddings, index=index)
 
-print(vectors, len(vectors))
+
